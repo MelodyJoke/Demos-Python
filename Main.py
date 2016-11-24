1b2daf2a959277d702d074daee70a2536da653af
MelodyJoke <melochanladyjj@gmail.com>
2016-11-24 12:55:01 +0800

    algorithms

--- Main.py
+++ Main.py
@@ -26,4 +26,3 @@ def euclid(left, right):
 inpLeft = int(input('type in two positive integers:\n'))
 inpRight = int(input(''))
 print('the greatest divisor of', inpLeft, 'and', inpRight, 'is', '%d.' % euclid(inpLeft, inpRight))
-print('hello')
